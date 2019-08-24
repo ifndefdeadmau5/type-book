8118b52a90ab4b1f1877345b06cefff8fefb64ee
Trevor <trevorme91@gmail.com>
2019-08-24 15:36:09 +0900

    Multiple words

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import TopMenu from './components/topMenu';
 import LinearDeterminate from './components/progress';  // progress bar
+import LinearProgress from '@material-ui/core/LinearProgress';  // progress bar
 
+const words = [
+'Apple',
+'Banana',
+'Carrot',
+'Grape',
+];
 function App() {
   const [userInput, setUserInput] = useState('type here');
+  const [index, setIndex] = useState(0);
   
   const sampleText = 'type this';
   const progressValue = 30;
@@ -13,16 +21,23 @@ function App() {
   return (
     <div className="App">
       <TopMenu />
-
-      <BookText text={sampleText} />
-      <input 
-        type="text" 
-        value={userInput} 
-        onChange={event => setUserInput(event.target.value)}
-      />
+      <LinearProgress value={index/words.length * 100} variant="determinate" /> 
+      <BookText text={words[index]} />
+      <form>
+        <input 
+          type="text" 
+          value={userInput} 
+          onChange={event => setUserInput(event.target.value)}
+        />
+        <button onClick={e => {
+          e.preventDefault();
+          setUserInput('');
+          setIndex(prev => prev + 1)
+        }}>
+          Submit
+        </button>
+      </form>
       {userInput === sampleText && <p>정확히 입력하셨습니다.</p>}
-
-      <LinearDeterminate value={progressValue} phone='01010'/> 
     </div>
   );
 }
